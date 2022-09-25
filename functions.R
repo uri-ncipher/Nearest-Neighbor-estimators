ead95953454e3b14844d7b464cdb5dafaf6a0125
tingfanglee <58047487+tingfanglee@users.noreply.github.com>
2022-09-25 16:20:06 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -34,20 +34,20 @@ pi=function(alpha){
 }
 Y_IPW=function(alpha, score){
   p=pi(alpha)
-    return(c(mean(data$outcome*data$treatment*p/score), 
+  return(c(mean(data$outcome*data$treatment*p/score), 
            mean(data$outcome*(1-data$treatment)*p/score), 
            mean(data$outcome*dbinom(data$treatment, 1, alpha)*p/score)))
 }
 Y_DE=function(alpha, score){
   p=pi(alpha)
   return(mean(data$outcome*data$treatment*p/score)- 
-         mean(data$outcome*(1-data$treatment)*p/score))
+           mean(data$outcome*(1-data$treatment)*p/score))
 }
 Y_IE=function(alpha, score){
   p0=pi(alpha[1])
   p1=pi(alpha[2])
   return(mean(data$outcome*(1-data$treatment)*p0/score)-
-  mean(data$outcome*(1-data$treatment)*p1/score))
+           mean(data$outcome*(1-data$treatment)*p1/score))
 }
 Y_TE=function(alpha, score){
   p0=pi(alpha[1])
@@ -58,8 +58,8 @@ Y_TE=function(alpha, score){
 Y_OE=function(alpha, score){
   p0=dbinom(data$treatment, 1, alpha[1])*pi(alpha[1])
   p1=dbinom(data$treatment, 1, alpha[2])*pi(alpha[2])
-  return(mean(data$outcome*(data$treatment)*p0/score)-
-           mean(data$outcome*(data$treatment)*p1/score))
+  return(mean(data$outcome*p0/score)-
+           mean(data$outcome*p1/score))
 }
 
 mat=function(theta){
@@ -267,18 +267,18 @@ IPW_1_model=function(data, base_covariate, alpha){
   score=propensity(c(fixef(M), as.data.frame(VarCorr(M))$sdcor))
   L=mat(c(fixef(M), as.data.frame(VarCorr(M))$sdcor))
   APO=rbind(cbind(ldply(alpha, Y_IPW, score=score), alpha=alpha, 
-                     type="point estimate"), 
-               cbind(ldply(alpha, Var, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                           grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                     alpha=alpha, type="variance"))
+                  type="point estimate"), 
+            cbind(ldply(alpha, Var, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
+                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
+                  alpha=alpha, type="variance"))
   names(APO)=c("a=1", "a=0", "margin", "alpha", "type")
   contrast=combinations(n=length(alpha), r=2, v=alpha)
   
   CE=rbind(cbind(ldply(alpha, Y_DE, score=score), 
                  alpha0=alpha, alpha1=alpha, type="Direct"), 
            cbind(ldply(alpha, Var_DE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                      grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                      alpha0=alpha, alpha1=alpha, type="Var DE"),
+                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+                 alpha0=alpha, alpha1=alpha, type="Var DE"),
            cbind(adply(contrast, 1, Y_IE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Indirect")[, -1],
            cbind(adply(contrast, 1, Var_IE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
@@ -288,13 +288,13 @@ IPW_1_model=function(data, base_covariate, alpha){
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Total")[, -1],
            cbind(adply(contrast, 1, Var_TE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1],
+                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var TE")[, -1],
            cbind(adply(contrast, 1, Y_OE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Overall")[, -1],
            cbind(adply(contrast, 1, Var_OE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1]
-           )
+                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var OE")[, -1]
+  )
   names(CE)=c("estimation", "alpha0", "alpha1", "type")
   return(list(APO, CE))
 }
@@ -371,12 +371,12 @@ IPW_2_model=function(data, M1,M2, alpha){
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Total")[, -1],
            cbind(adply(contrast, 1, Var_TE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1],
+                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var TE")[, -1],
            cbind(adply(contrast, 1, Y_OE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Overall")[, -1],
            cbind(adply(contrast, 1, Var_OE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
-                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1]
+                 alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var OE")[, -1]
   )
   names(CE)=c("estimation", "alpha0", "alpha1", "type")
   return(list(APO, CE))
