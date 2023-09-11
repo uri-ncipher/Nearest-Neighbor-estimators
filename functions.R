e79a0f03204d084836424107b85cd2d3052ec841
tingfanglee <58047487+tingfanglee@users.noreply.github.com>
2023-09-10 22:29:08 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -12,20 +12,17 @@ avg_neighbors=function(net, node, variable){
 ################## IPW1 functions ################################################
 ##################################################################################
 propensity=function(theta){
-  prop=c()
-  for (i in 1:n){
+  prop = foreach(i = 1:n, .combine = "c") %dopar% {
     subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
     integrand=function(b){
       p=1
       for (j in 1:nrow(subdata)){
         pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
-        if (subdata$treatment[j]==1){
-          p=p*plogis(as.vector(pred)+as.vector(b))
-        } else {p=p*(1-plogis(as.vector(pred)+as.vector(b)))}
+        p=p*dbinom(subdata$treatment[j], size = 1, prob = plogis(as.vector(pred)+as.vector(b)))
       }
       return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
     }
-    prop=c(prop, integrate(integrand, -Inf, Inf)$value)
+    integrate(integrand, -Inf, Inf)$value
   }
   return(prop)
 }
@@ -60,57 +57,6 @@ Y_OE=function(alpha, score){
   p1=dbinom(data$treatment, 1, alpha[2])*pi(alpha[2])
   return(mean(data$outcome*p0/score)-
            mean(data$outcome*p1/score))
-}
-
-mat=function(theta){
-  log_propensity_ind=function(theta){
-    subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
-    integrand=function(b){
-      p=1
-      for (j in 1:nrow(subdata)){
-        pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
-        if (subdata$treatment[j]==1){
-          p=p*plogis(as.vector(pred)+as.vector(b))
-        } else {p=p*(1-plogis(as.vector(pred)+as.vector(b)))}
-      }
-      return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
-    }
-    return(log(integrate(integrand, -Inf, Inf)$value))
-  }
-  propensity_ind=function(theta){
-    subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
-    integrand=function(b){
-      p=1
-      for (j in 1:nrow(subdata)){
-        pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
-        if (subdata$treatment[j]==1){
-          p=p*plogis(as.vector(pred)+as.vector(b))
-        } else {p=p*(1-plogis(as.vector(pred)+as.vector(b)))}
-      }
-      return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
-    }
-    return(integrate(integrand, -Inf, Inf)$value)
-  }
-  propensity_ind_inv=function(theta){
-    return(1/propensity_ind(theta))
-  }
-  U_11=matrix(0, nrow = length(theta), ncol = length(theta))
-  v_11=matrix(NA, nrow=n, ncol = length(theta))
-  grad_propensity_inv=matrix(NA, nrow = n, ncol = length(theta) )
-  for (i in 1:n){
-    U_11=U_11-(1/n)*hessian(log_propensity_ind, x=theta)
-    v_11[i, ]=grad(log_propensity_ind, x=theta)
-    grad_propensity_inv[i, ]=grad(propensity_ind_inv, x=theta)
-  }
-  
-  V_11=matrix(0, nrow = length(theta), ncol =length(theta) )
-  for (j in 1:m){
-    V_11=V_11+colSums(v_11[data$id[data$component==j],])%*%t(colSums(v_11[data$id[data$component==j],]))
-  }
-  V_11=V_11*(m/n^2)
-  
-  return(list(U_11, v_11, V_11, grad_propensity_inv))
-  
 }
 
 Var_Y=function(a, alpha, U_11, v_11, V_11, grad_propensity_inv, score, theta){
@@ -265,34 +211,87 @@ IPW_1_model=function(data, base_covariate, alpha){
   M=glmer(formula, data = nn_data, family = binomial(link= "logit"))
   theta=c(fixef(M), as.data.frame(VarCorr(M))$sdcor)
   score=propensity(c(fixef(M), as.data.frame(VarCorr(M))$sdcor))
-  L=mat(c(fixef(M), as.data.frame(VarCorr(M))$sdcor))
+  U_11 = foreach(i=1:n, .combine = "+") %dopar% {
+    log_propensity_ind=function(theta){
+      subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
+      integrand=function(b){
+        p=1
+        for (j in 1:nrow(subdata)){
+          pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
+          p=p*dbinom(subdata$treatment[j], size = 1, prob = plogis(as.vector(pred)+as.vector(b)))
+        }
+        return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
+      }
+      return(log(integrate(integrand, -Inf, Inf)$value))
+    }
+    -(1/n)*hessian(log_propensity_ind, x=theta)
+  }
+  v_11 = foreach(i=1:n, .combine = "rbind") %dopar% {
+    log_propensity_ind=function(theta){
+      subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
+      integrand=function(b){
+        p=1
+        for (j in 1:nrow(subdata)){
+          pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
+          p=p*dbinom(subdata$treatment[j], size = 1, prob = plogis(as.vector(pred)+as.vector(b)))
+        }
+        return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
+      }
+      return(log(integrate(integrand, -Inf, Inf)$value))
+    }
+    grad(log_propensity_ind, x=theta)
+  }
+  grad_propensity_inv = foreach (i = 1:n, .combine = "rbind") %dopar% {
+    propensity_ind_inv=function(theta){
+      subdata=subset(data, data$id %in% neighborhood(net0, order = 1, nodes = i)[[1]])
+      integrand=function(b){
+        p=1
+        for (j in 1:nrow(subdata)){
+          pred=c(theta[1], theta[2:(2+length(base_covariate)-1)]) %*% c(1, unlist(subdata[j, base_covariate]))
+          p=p*dbinom(subdata$treatment[j], size = 1, prob = plogis(as.vector(pred)+as.vector(b)))
+        }
+        return(p*dnorm(b, mean = 0, sd=theta[length(theta)]))
+      }
+      return(1/integrate(integrand, -Inf, Inf)$value)
+    }
+    grad(propensity_ind_inv, x=theta)
+  }
+  V_11 = foreach (j = 1:m, .combine = "+") %do% {
+    colSums(v_11[data$id[data$component==j],])%*%t(colSums(v_11[data$id[data$component==j],]))
+  }
+  V_11=V_11*(m/n^2)
+  
   APO=rbind(cbind(ldply(alpha, Y_IPW, score=score), alpha=alpha, 
                   type="point estimate"), 
-            cbind(ldply(alpha, Var, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
+            cbind(ldply(alpha, Var, U_11, v_11, V_11, 
+                        grad_propensity_inv, score=score, theta=theta), 
                   alpha=alpha, type="variance"))
   names(APO)=c("a=1", "a=0", "margin", "alpha", "type")
   contrast=t(combn(alpha,2)) 
   
   CE=rbind(cbind(ldply(alpha, Y_DE, score=score), 
                  alpha0=alpha, alpha1=alpha, type="Direct"), 
-           cbind(ldply(alpha, Var_DE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(ldply(alpha, Var_DE, U_11=U_11, v_11=v_11, V_11=V_11, 
+                       grad_propensity_inv=grad_propensity_inv, score=score, 
+                       theta=theta), 
                  alpha0=alpha, alpha1=alpha, type="Var DE"),
            cbind(adply(contrast, 1, Y_IE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Indirect")[, -1],
-           cbind(adply(contrast, 1, Var_IE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_IE, U_11=U_11, v_11=v_11, V_11=V_11, 
+                       grad_propensity_inv=grad_propensity_inv, score=score, 
+                       theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1],
            cbind(adply(contrast, 1, Y_TE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Total")[, -1],
-           cbind(adply(contrast, 1, Var_TE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_TE, U_11=U_11, v_11=v_11, V_11=V_11, 
+                       grad_propensity_inv=grad_propensity_inv, score=score, 
+                       theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var TE")[, -1],
            cbind(adply(contrast, 1, Y_OE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Overall")[, -1],
-           cbind(adply(contrast, 1, Var_OE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_OE, U_11=U_11, v_11=v_11, V_11=V_11, 
+                       grad_propensity_inv=grad_propensity_inv, score=score, 
+                       theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var OE")[, -1]
   )
   names(CE)=c("estimation", "alpha0", "alpha1", "type")
@@ -308,74 +307,79 @@ propensity_2=function(M1, M2){
   p2=predict(M2, data, type = "response")
   return((p1^data$na_a)*((1-p1)^data$notna_a)*dbinom(data$treatment, size = 1, prob = p2))
 }
-mat_2=function(theta){
-  propensity_2_ind=function(theta){
-    v1=theta[1:length(M1$coefficients)]
-    v2=theta[(length(M1$coefficients)+1):length(theta)]
-    p1=plogis(v1 %*% unlist(c(1, data[i, c("treatment", base_covariate, avg_covariate)])))
-    p2=plogis(v2 %*% unlist(c(1, data[i, base_covariate])))
-    prop=(p1^data$na_a[i])*((1-p1)^data$notna_a[i])*dbinom(data$treatment[i], size = 1, prob = p2)
-    return(prop)
-  }
-  log_propensity_2_ind=function(theta){
-    return(log(propensity_2_ind(theta)))
-  }
-  propensity_2_ind_inv=function(theta){
-    return(1/propensity_2_ind(theta))
-  }
-  
-  U_11=matrix(0, nrow = length(theta), ncol = length(theta))
-  v_11=matrix(NA, nrow=n, ncol = length(theta))
-  grad_propensity_inv_2=matrix(NA, nrow = n, ncol = length(theta) )
-  for (i in 1:n){
-    U_11=U_11-(1/n)*hessian(log_propensity_2_ind, x=theta)
-    v_11[i, ]=grad(log_propensity_2_ind, x=theta)
-    grad_propensity_inv_2[i, ]=grad(propensity_2_ind_inv, x=theta)
-  }
-  
-  V_11=matrix(0, nrow = length(theta), ncol =length(theta) )
-  for (j in 1:m){
-    V_11=V_11+colSums(v_11[data$id[data$component==j],])%*%t(colSums(v_11[data$id[data$component==j],]))
-  }
-  V_11=V_11*(m/n^2)
-  
-  return(list(U_11, v_11, V_11, grad_propensity_inv_2))
-}
-
 
 IPW_2_model=function(data, M1,M2, alpha){
   
   score=propensity_2(M1, M2)
   theta=c(M1$coefficients, M2$coefficients)
-  L=mat_2(theta)
   
+  U_11 = foreach(i = 1:n, .combine = "+") %dopar% {
+    log_propensity_2_ind=function(theta){
+      v1=theta[1:length(M1$coefficients)]
+      v2=theta[(length(M1$coefficients)+1):length(theta)]
+      p1=plogis(v1 %*% unlist(c(1, data[i, c("treatment", base_covariate, avg_covariate)])))
+      p2=plogis(v2 %*% unlist(c(1, data[i, base_covariate])))
+      #prop=dbinom(data$na_a[i], size = data$na[i], prob = p1)*dbinom(data$treatment[i], size = 1, prob = p2)
+      prop=(p1^data$na_a[i])*((1-p1)^data$notna_a[i])*dbinom(data$treatment[i], size = 1, prob = p2)
+      return(log(prop))
+    }
+    -(1/n)*hessian(log_propensity_2_ind, x=theta)
+  }
+  v_11 = foreach(i = 1:n, .combine = "rbind") %dopar% {
+    log_propensity_2_ind=function(theta){
+      v1=theta[1:length(M1$coefficients)]
+      v2=theta[(length(M1$coefficients)+1):length(theta)]
+      p1=plogis(v1 %*% unlist(c(1, data[i, c("treatment", base_covariate, avg_covariate)])))
+      p2=plogis(v2 %*% unlist(c(1, data[i, base_covariate])))
+      #prop=dbinom(data$na_a[i], size = data$na[i], prob = p1)*dbinom(data$treatment[i], size = 1, prob = p2)
+      prop=(p1^data$na_a[i])*((1-p1)^data$notna_a[i])*dbinom(data$treatment[i], size = 1, prob = p2)
+      return(log(prop))
+    }
+    grad(log_propensity_2_ind, x=theta)
+  }
+  grad_propensity_inv = foreach (i = 1:n, .combine = "rbind") %dopar% {
+    propensity_2_ind_inv = function(theta){
+      v1=theta[1:length(M1$coefficients)]
+      v2=theta[(length(M1$coefficients)+1):length(theta)]
+      p1=plogis(v1 %*% unlist(c(1, data[i, c("treatment", base_covariate, avg_covariate)])))
+      p2=plogis(v2 %*% unlist(c(1, data[i, base_covariate])))
+      #prop=dbinom(data$na_a[i], size = data$na[i], prob = p1)*dbinom(data$treatment[i], size = 1, prob = p2)
+      prop=(p1^data$na_a[i])*((1-p1)^data$notna_a[i])*dbinom(data$treatment[i], size = 1, prob = p2)
+      return(1/prop)
+    }
+    grad(propensity_2_ind_inv, x=theta)
+  }
+  V_11 = foreach(j = 1:m, .combine = "+") %dopar% {
+    colSums(v_11[data$id[data$component==j],])%*%t(colSums(v_11[data$id[data$component==j],]))
+  }
+  V_11=V_11*(m/n^2)
   APO=rbind(cbind(ldply(alpha, Y_IPW, score=score), alpha=alpha, 
                   type="point estimate"), 
-            cbind(ldply(alpha, Var, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                        grad_propensity_inv=L[[4]], score=score, theta=theta), 
+            cbind(ldply(alpha, Var, U_11, v_11, V_11, 
+                        grad_propensity_inv, score=score, theta=theta), 
                   alpha=alpha, type="variance"))
   names(APO)=c("a=1", "a=0", "margin", "alpha", "type")
   contrast=t(combn(alpha,2)) 
   
   CE=rbind(cbind(ldply(alpha, Y_DE, score=score), 
                  alpha0=alpha, alpha1=alpha, type="Direct"), 
-           cbind(ldply(alpha, Var_DE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(ldply(alpha, Var_DE, U_11, v_11, V_11, 
+                       grad_propensity_inv, score=score, theta=theta), 
                  alpha0=alpha, alpha1=alpha, type="Var DE"),
            cbind(adply(contrast, 1, Y_IE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Indirect")[, -1],
-           cbind(adply(contrast, 1, Var_IE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_IE, U_11, v_11, V_11, 
+                       grad_propensity_inv, score=score, theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var IE")[, -1],
            cbind(adply(contrast, 1, Y_TE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Total")[, -1],
-           cbind(adply(contrast, 1, Var_TE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_TE, U_11, v_11, V_11, 
+                       grad_propensity_inv, score=score, theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var TE")[, -1],
            cbind(adply(contrast, 1, Y_OE, score=score), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Overall")[, -1],
-           cbind(adply(contrast, 1, Var_OE, U_11=L[[1]], v_11=L[[2]], V_11=L[[3]], 
-                       grad_propensity_inv=L[[4]], score=score, theta=theta), 
+           cbind(adply(contrast, 1, Var_OE, U_11, v_11, V_11, 
+                       grad_propensity_inv, score=score, theta=theta), 
                  alpha0=contrast[, 1], alpha1=contrast[, 2], type="Var OE")[, -1]
   )
   names(CE)=c("estimation", "alpha0", "alpha1", "type")
